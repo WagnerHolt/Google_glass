9a4ef990f8920d4ed24ada59755a181d241152bc
Carlos Wagner <carlosconceicao-@hotmail.com>
2020-01-20 13:04:06 -0300

    Primeiro commit

---
+++ _javascript/funcoes.js
@@ -0,0 +1,7 @@
+/*function mudaFoto(foto){
+			document.getElementById("icone").src = "_imagens/"+ foto +".png";
+		};
+*/
+function mudaFoto (foto) {
+			document.getElementById("icone").src = foto;
+		}
